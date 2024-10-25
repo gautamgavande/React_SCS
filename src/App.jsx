495dac9dc8967eb6748831cb0324d7e4e87819cc
gautamgavande <gautamgavande2436@gmail.com>
2024-10-25 13:50:32 +0530

    add redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,38 @@ import viteLogo from '/vite.svg'
 import Card1 from './Components/Card1'
 import Card2 from './Components/Card2'
 import Card4 from './Components/Card4'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment } from './features/counter/counterSlice'
 // import Card from './Components/Card'
 
 function App() {
+  const count=useSelector((state)=>state.counter.value)
+  const dispatch=useDispatch()
+
+
+  function handleIncrementClick(){
+         dispatch(increment())
+  }
+
+  function handleDecrimentClick(){
+          dispatch(decrement())
+  }
   return (
     <>
-    <Card4  text="Download" color="bg-red-600"></Card4>
-    <Card4 text="Know More" color="bg-blue-600"></Card4>
+      <div className='container'>
+
+        <button onClick={handleIncrementClick}>+</button>
+        <p className='font-bold'>Count:{count}</p>
+        <button onClick={handleDecrimentClick}>-</button>
+
+      </div>
+
+
+
+
+   {/* ////////////////////////////// */}
+    {/* <Card4  text="Download" color="bg-red-600"></Card4> */}
+    {/* <Card4 text="Know More" color="bg-blue-600"></Card4> */}
     {/* <Card2></Card2> */}
       {/* <Card1></Card1> */}
       {/* <Card></Card> */}
